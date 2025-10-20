24ae1d560ab331dc6a117a2b75605256a8a80247
123 <21@123>
2025-10-20 16:25:48 +0800

    Json and txt log file analysis

--- get_res.py
+++ get_res.py
@@ -38,52 +38,20 @@ def collect_results_all_tasks(base_dir):
             pre_train_log = os.path.join(setting_path, 'pre_train', 'log_best.txt')
             pre_teacher_map50 = extract_map50_from_log(pre_train_log, 'best_teacher')
             self_model_map50 = extract_map50_from_json(os.path.join(setting_path, 'self_train', 'result_summary.json'))
-            # self_model_map50_3 = extract_map50_from_json(os.path.join(setting_path, 'self_train_thr_0.3', 'result_summary.json'))
-            # self_model_map50_4 = extract_map50_from_json(os.path.join(setting_path, 'self_train_thr_0.4', 'result_summary.json'))
-            # self_model_map50_5 = extract_map50_from_json(os.path.join(setting_path, 'self_train_thr_0.5', 'result_summary.json'))
-            # self_model_map50_5 = extract_map50_from_json(os.path.join(setting_path, 'self_train_thr_0.6', 'result_summary.json'))
 
             all_results.append({
                 'task': task,
                 'setting': setting,
                 'pre_train_best_teacher_map50': pre_teacher_map50,
                 'self_model_map50': self_model_map50,
-                # 'self_train_thr_0.3': self_model_map50_3,
-                # 'self_train_thr_0.4': self_model_map50_4,
-                # 'self_train_thr_0.5': self_model_map50_5,
-                # 'self_train_thr_0.6': self_model_map50_5,
             })
 
 
     return pd.DataFrame(all_results)
 
-def collect_results_all_tasks_teacher(base_dir):
-    all_results = []
-
-    for task in os.listdir(base_dir):  # 遍历如 DINO_HRRSD_to_SSDD
-        task_path = os.path.join(base_dir, task)
-        if not os.path.isdir(task_path):
-            continue
-
-        for setting in os.listdir(task_path):  # 遍历具体实验配置
-            setting_path = os.path.join(task_path, setting)
-            if not os.path.isdir(setting_path):
-                continue
-
-            # 日志路径
-            self_model_map50 = extract_map50_from_json(os.path.join(setting_path, 'result_summary.json'))
-            if self_model_map50 is not None:
-                all_results.append({
-                    'task': task,
-                    'setting': setting,
-                    'self_model_map50': self_model_map50,
-                })
-
-    return pd.DataFrame(all_results)
-
 
 if __name__ == "__main__":
-    root_dir = 'reproduction/'
+    root_dir = ' '
     df = collect_results_all_tasks(root_dir)
     df.to_excel('experiment_summary.xlsx', index=False)  # 保存为Excel
-    print("实验结果已保存为 experiment_summary.xlsx")
+    # print("实验结果已保存为 experiment_summary.xlsx")
